313ed7a38ab0e1db08c74a16a228c515b579893c
Harold GR <harold@girres.com.co>
2024-10-05 13:29:09 +0200

    Zona Herragro

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -41,31 +41,45 @@ export default function Page({ params = {} }) {
 
   return (
     <main className='project-page site-container min-h-screen py-24 text-main-gray'>
-      <div className='breadcrumbs text-xl'>
+      <div className='breadcrumbs text-xl fontLight'>
         <ul>
-          <li className='fontLight'>
+          <li>
             <Link href='/'>Home</Link>
           </li>
-          <li className='fontBold'>Projects</li>
+          <li className='text-main-white'>Projects</li>
         </ul>
       </div>
-      <h1 className='my-10 text-main-white'>{project.title}</h1>
-      <div className='project-header grid grid-cols-12'>
-        <div className='project-info col-span-12 lg:col-span-4'>
-          <h2>{project.caption}</h2>
-          <div className='flex flex-col gap-2 items-start mt-10'>
-            {project.tags.map((tag) => (
-              <p
-                key={tag}
-                className='border-[1px] border-main-gray rounded-full text-sm p-2 px-5'
-              >
-                {tag}
-              </p>
-            ))}
-          </div>
+      <div></div>
+      <div className='project-header'>
+        <div className='project-title flex items-center justify-between'>
+          <h1 className='my-10 text-main-white'>{project.title}</h1>
+          {project.website && (
+            <Link
+              href={project.website}
+              className='btn btn-sm rounded-full bg-transparent text-main-white border-main-white text-xs hover:bg-main-dark hover:scale-110'
+            >
+              Visit Website
+              <MdChevronRight className='h-4 w-4' />
+            </Link>
+          )}
         </div>
-        <div className='project-images col-span-12 lg:col-span-8'>
-          <Gallery images={project.images} name={project.title} />
+        <div className='grid grid-cols-12'>
+          <div className='project-info col-span-12 lg:col-span-4'>
+            <h2>{project.caption}</h2>
+            <div className='flex flex-col gap-2 items-start mt-10'>
+              {project.tags.map((tag) => (
+                <p
+                  key={tag}
+                  className='border-[1px] border-main-gray rounded-full text-sm p-2 px-5'
+                >
+                  {tag}
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className='project-images col-span-12 lg:col-span-8'>
+            <Gallery images={project.images} name={project.title} />
+          </div>
         </div>
       </div>
       <div className='project-about mt-10 text-sm grid grid-cols-12 pt-20'>
--- src/projectsData.js
+++ src/projectsData.js
@@ -15,13 +15,38 @@ const data = [
       'Throughout the project, I worked closely with the development team to ensure that the design vision was successfully translated into a functional and responsive platform. This collaboration was crucial in optimizing both the front-end and back-end user experience, ensuring the interfaces were both visually appealing and technically sound.',
     ],
     slug: 'reteki',
-    next: '/saudia-beyond',
+    next: '/zona-herragro',
     images: [
       '/images/projects/reteki/1.jpg',
       '/images/projects/reteki/2.jpg',
       '/images/projects/reteki/3.jpg',
     ],
     aboutImages: ['/images/projects/reteki/4.jpg'],
+  },
+  {
+    title: 'Zona Herragro',
+    caption: 'E-Commerce - 2023',
+    tags: [
+      'E-commerce',
+      'User Research',
+      'User Experience Design',
+      'Interaction Design',
+      'Graphics Design',
+    ],
+    about: [
+      'Facing operational challenges in logistics and sales, our client realized their outdated systems were holding them back. They turned to us for a technological upgrade, and we implemented the right solutions to drive efficiency and push their business forward.',
+      'As part of this transformation, we designed the UX/UI for their e-commerce platform, creating a seamless and intuitive user experience. This was a private app and web solution, exclusively accessible to customers and employees. The platform facilitated smooth internal and external interactions, optimizing operations and improving overall productivity.',
+      'Additionally, we designed motion graphics for the brand, adding a dynamic and engaging visual layer to their digital presence, enhancing user engagement and reinforcing brand identity.        ',
+    ],
+    slug: 'zona-herragro',
+    next: '/saudia-beyond',
+    images: [
+      '/images/projects/zonaHerragro/1.jpg',
+      '/images/projects/zonaHerragro/2.jpg',
+      '/images/projects/zonaHerragro/3.jpg',
+    ],
+    aboutImages: ['/images/projects/zonaHerragro/4.jpg'],
+    website: 'https://www.zonaherragro.com',
   },
 ];
 
