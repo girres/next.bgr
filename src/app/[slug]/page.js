20f85831a6379fe7dfe8437f506bfb58b8c66a4d
Harold GR <harold@girres.com.co>
2024-10-05 13:20:03 +0200

    Reteki

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,6 +1,7 @@
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
 import data from '@/projectsData';
+import { MdChevronRight } from 'react-icons/md';
 
 // Components
 import { Gallery } from '@/components/Projects';
@@ -39,31 +40,61 @@ export default function Page({ params = {} }) {
   if (!slug || !project || !project?.slug) return notFound();
 
   return (
-    <main className='project-page site-containers px-5 min-h-screen py-24 bg-purple-900'>
+    <main className='project-page site-container min-h-screen py-24 text-main-gray'>
+      <div className='breadcrumbs text-xl'>
+        <ul>
+          <li className='fontLight'>
+            <Link href='/'>Home</Link>
+          </li>
+          <li className='fontBold'>Projects</li>
+        </ul>
+      </div>
+      <h1 className='my-10 text-main-white'>{project.title}</h1>
       <div className='project-header grid grid-cols-12'>
-        <div className='project-info col-span-12 lg:col-span-5 bg-pink-400'>
-          <h1>{project.title}</h1>
+        <div className='project-info col-span-12 lg:col-span-4'>
           <h2>{project.caption}</h2>
-          <div className='tags space-y-2'>
+          <div className='flex flex-col gap-2 items-start mt-10'>
             {project.tags.map((tag) => (
-              <div key={tag}>{tag}</div>
+              <p
+                key={tag}
+                className='border-[1px] border-main-gray rounded-full text-sm p-2 px-5'
+              >
+                {tag}
+              </p>
             ))}
           </div>
         </div>
-        <div className='project-images col-span-12 lg:col-span-7 bg-cyan-400'>
+        <div className='project-images col-span-12 lg:col-span-8'>
           <Gallery images={project.images} name={project.title} />
         </div>
       </div>
-      <div>
-        <div className='breadcrumbs text-sm'>
-          <ul>
-            <li>
-              <Link href='/'>Home</Link>
-            </li>
-            <li>Reteki</li>
-          </ul>
+      <div className='project-about mt-10 text-sm grid grid-cols-12 pt-20'>
+        <div className='col-span-12 lg:col-span-7 lg:pr-10'>
+          <h2>
+            <span className='text-main-white'>About</span>
+            {` `}the Project
+          </h2>
+          {project.about.map((paragraph, index) => (
+            <p key={index} className='my-5'>
+              {paragraph}
+            </p>
+          ))}
+        </div>
+        <div className='col-span-12 lg:col-span-5'>
+          <Gallery images={project.aboutImages} name={project.title} />
         </div>
       </div>
+      {project.next && (
+        <div className='next-project mt-20'>
+          <Link
+            href={project.next}
+            className='btn btn-sm rounded-full bg-transparent text-main-white border-main-white text-xs hover:bg-main-dark hover:scale-110'
+          >
+            Next Project
+            <MdChevronRight className='h-4 w-4' />
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
