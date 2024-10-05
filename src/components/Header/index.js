d2208a5fd2c1886066af25bb86f9f4ea007d0ac1
Harold GR <harold@girres.com.co>
2024-10-05 14:57:22 +0200

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import { MdEmail } from 'react-icons/md';
 
 const Component = () => {
   return (
-    <header className='bg-main-purple fixed w-full z-50 py-3'>
+    <header className='fixed w-full z-50 py-3'>
       <div className='site-container flex items-center justify-between'>
         <div className='lg:flex items-center space-x-4'>
           <Link href='/'>
