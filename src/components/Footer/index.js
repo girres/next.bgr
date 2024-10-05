3d2fd8670dca2bee641cc96eabf7c5b5af60114b
Harold GR <harold@girres.com.co>
2024-10-05 15:38:03 +0200

    Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,13 @@ import Link from 'next/link';
 const Component = () => {
   return (
     <footer>
-      <div className='site-container py-20 border-t-2 border-t-main-gray text-main-gray'>
-        <div className='grid grid-cols-2 items-end'>
-          <p className='textLight'>{new Date().getFullYear()} &copy; bryangr</p>
-          <div className=''>
-            <nav className='flex items-center justify-end space-x-8 text-4xl'>
+      <div className='site-container py-10 border-t-2 border-t-main-gray/70 text-main-gray'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 items-end'>
+          <p className='order-2 lg:order-1 textLight text-center lg:text-left mt-5 lg:mt-0'>
+            {new Date().getFullYear()} &copy; bryangr
+          </p>
+          <div className='order-1 lg:order-2'>
+            <nav className='flex items-center justify-center lg:justify-end space-x-8 text-2xl lg:text-4xl'>
               <Link
                 href='mailto:hello@bryangr.com'
                 target='_blank'
